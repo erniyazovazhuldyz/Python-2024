8d37926efd8e8bab98fd2b1ad2444b292eead4d9
erniyazovazhuldyz <157003944+erniyazovazhuldyz@users.noreply.github.com>
2024-02-01 16:34:51 +0600

    Add files via upload

---
+++ lab3/Classes and Objects.py
@@ -0,0 +1,20 @@
+#exercise 1
+class MyClass:
+    x = 5
+ 
+#exercise 2
+class MyClass:
+    x = 5
+    p1 = MyClass()
+
+#exercise 3
+class MyClass:
+    x = 5
+    p1 = MyClass()
+    print(p1.x)
+
+#exercise 4
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
